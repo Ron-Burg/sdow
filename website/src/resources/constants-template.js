29d4245b9fbef8769b35b1f0af563639eea8e70f
Ron Burg <Ron.Burg31@gmail.com>
2019-05-02 18:19:42 +0300

    Removing IP and DNS for security

---
+++ website/src/resources/constants-template.js
@@ -0,0 +1,8 @@
+// Replace IP With the correct one for your env
+const SDOW_API_URL_DEV = 'http://IP:5000';
+const SDOW_API_URL_PROD = 'http://IP:8888';
+
+export const SDOW_API_URL =
+  process.env.NODE_ENV === 'production' ? SDOW_API_URL_PROD : SDOW_API_URL_DEV;
+
+export const WIKIPEDIA_API_URL = 'https://he.wikipedia.org/w/api.php';
