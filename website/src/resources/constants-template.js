130a67e14bf2e51312cd8aa87715464e8caa5f4e
Ron Burg <Ron.Burg31@gmail.com>
2019-05-02 18:46:43 +0300

    returning to en.wiki insted of he.wiki

--- website/src/resources/constants-template.js
+++ website/src/resources/constants-template.js
@@ -5,4 +5,4 @@ const SDOW_API_URL_PROD = 'http://IP:8888';
 export const SDOW_API_URL =
   process.env.NODE_ENV === 'production' ? SDOW_API_URL_PROD : SDOW_API_URL_DEV;
 
-export const WIKIPEDIA_API_URL = 'https://he.wikipedia.org/w/api.php';
+export const WIKIPEDIA_API_URL = 'https://en.wikipedia.org/w/api.php';
